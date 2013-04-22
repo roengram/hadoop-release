4f7836e7658e594262c68300f595ae9a29e7d7ac
Suresh Srinivas <sureshms@yahoo-inc.com>
2013-04-22 06:51:14 -0700

    NFS: add common NFS and mountd packages

---
+++ src/core/org/apache/hadoop/nfs/nfs3/request/WRITE3Request.java
@@ -0,0 +1,74 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.nfs.nfs3.request;
+
+import java.io.IOException;
+
+import org.apache.hadoop.nfs.nfs3.FileHandle;
+import org.apache.hadoop.nfs.nfs3.Nfs3Constant.WriteStableHow;
+import org.apache.hadoop.oncrpc.XDR;
+
+public class WRITE3Request extends RequestWithHandle {
+  private final long offset;
+  private final int count;
+  private final WriteStableHow stableHow;
+  private final byte[] data;
+
+  public WRITE3Request(XDR xdr) throws IOException {
+    super(xdr);
+    offset = xdr.readHyper();
+    count = xdr.readInt();
+    stableHow = WriteStableHow.fromValue(xdr.readInt());
+    data = xdr.readFixedOpaque(xdr.readInt());
+  }
+
+  public WRITE3Request(FileHandle handle, final long offset, final int count,
+      final WriteStableHow stableHow, final byte[] data) {
+    super(handle);
+    this.offset = offset;
+    this.count = count;
+    this.stableHow = stableHow;
+    this.data = data;
+  }
+
+  public long getOffset() {
+    return this.offset;
+  }
+
+  public int getCount() {
+    return this.count;
+  }
+
+  public WriteStableHow getStableHow() {
+    return this.stableHow;
+  }
+
+  public byte[] getData() {
+    return this.data;
+  }
+
+  @Override
+  public void serialize(XDR xdr) {
+    handle.serialize(xdr);
+    xdr.writeLongAsHyper(offset);
+    xdr.writeInt(count);
+    xdr.writeInt(stableHow.getValue());
+    xdr.writeInt(count);
+    xdr.writeFixedOpaque(data, count);
+  }
+}
