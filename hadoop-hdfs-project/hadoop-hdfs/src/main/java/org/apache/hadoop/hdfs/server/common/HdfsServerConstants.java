ff557b76efc887c00664c63b4b58714758208690
Tsz-Wo Nicholas Sze <szetszwo@hortonworks.com>
2014-02-27 13:56:47 -0800

    Revert "HDFS-5835. Add a new option for starting Namenode when rolling upgrade is in progress."
    This reverts commit d79dfdd76cf5fb3895f9568802367706cf84a0e9.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/HdfsServerConstants.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/HdfsServerConstants.java
@@ -47,7 +47,7 @@ public final class HdfsServerConstants {
 
   /** Startup options for rolling upgrade. */
   public static enum RollingUpgradeStartupOption{
-    ROLLBACK, DOWNGRADE, STARTED;
+    ROLLBACK, DOWNGRADE;
     
     private static final RollingUpgradeStartupOption[] VALUES = values();
 
