3d265caf2c9178f408cf938fbbee8eaba49034fd
Suresh Srinivas <sureshms@yahoo-inc.com>
2014-04-05 23:11:53 -0700

    BUG-16235. Azure file system unit tests fail due to invalid file names. Contributed by Chris Nauroth.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/azurenative/TestNativeAzureFileSystemOperationsMocked.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/azurenative/TestNativeAzureFileSystemOperationsMocked.java
@@ -6,6 +6,9 @@ import org.apache.hadoop.fs.*;
 public class TestNativeAzureFileSystemOperationsMocked extends
   FSMainOperationsBaseTest {
 
+  private static final String TEST_ROOT_DIR =
+    "/tmp/TestNativeAzureFileSystemOperationsMocked";
+
   @Override
   protected FileSystem createFileSystem() throws Exception {
     return AzureBlobStorageTestAccount.createMock().getFileSystem();
@@ -17,5 +20,30 @@ public class TestNativeAzureFileSystemOperationsMocked extends
         "Skipping testListStatusThrowsExceptionForUnreadableDir since WASB" +
         " doesn't honor directory permissions.");
     assumeTrue(!Path.WINDOWS);
+  }
+
+  @Override
+  public String getTestRootDir() {
+    return TEST_ROOT_DIR;
+  }
+
+  @Override
+  public Path getTestRootPath(FileSystem fSys) {
+    return fSys.makeQualified(new Path(TEST_ROOT_DIR));
+  }
+
+  @Override
+  public Path getTestRootPath(FileSystem fSys, String pathString) {
+    return fSys.makeQualified(new Path(TEST_ROOT_DIR, pathString));
+  }
+
+  @Override
+  public Path getAbsoluteTestRootPath(FileSystem fSys) {
+    Path testRootPath = new Path(TEST_ROOT_DIR);
+    if (testRootPath.isAbsolute()) {
+      return testRootPath;
+    } else {
+      return new Path(fSys.getWorkingDirectory(), TEST_ROOT_DIR);
+    }
   }
 }
