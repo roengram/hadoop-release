0d79d754f7ae3f200ceae30b9e65fd601ac7a5d3
Ivan Malamen <malamen@ukr.net>
2014-06-12 16:25:30 +0300

    BUG-16991 No GC log for NameNode on Windows

--- hadoop-dist/src/win/scripts/install.ps1
+++ hadoop-dist/src/win/scripts/install.ps1
@@ -271,6 +271,14 @@ function Main( $scriptDir )
     }
 
     ###
+    ### Modifying hadoop-env.cmd to support Namenode GC
+    ###
+    Write-Log "Modifying hadoop-env.cmd to support Namenode GC"
+    $file =  "$hadoopInstallDir\etc\hadoop\hadoop-env.cmd"
+    $line = "`nset HADOOP_NAMENODE_OPTS==-Xloggc:%HADOOP_LOG_DIR%/gc-namenode.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps %HADOOP_NAMENODE_OPTS%"
+    Add-Content $file $line
+
+    ###
     ### Install and Configure HDFS
     ###
 
